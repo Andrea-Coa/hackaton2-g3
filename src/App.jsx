e16606cfc3c6898d33a0051143937fa29af406e0
AndreaCoa <andrea.coa@utec.edu.pe>
2024-06-22 11:37:45 -0500

    template

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <Router>
+    <Routes>
+      <Route path="/" element={<Navigate to= 'auth/login' />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/auth/login" element={<Login />} />
+      <Route path="/auth/register" element={<Register />} />
+      <Route path="/auth/register" element={<Register />} />
+
+      <Route path='*' element={<div>Not found</div>}/>
+    </Routes>
+   </Router>
+  )
+}
+
+export default App
